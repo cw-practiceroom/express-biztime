aaaf858150063a0bbb6aacd2bf1c4f0b1c776170
Caleb Waymeyer <cjwaymeyer@gmail.com>
2021-10-13 11:24:44 -0400

    functional

--- routes/companies.js
+++ routes/companies.js
@@ -6,7 +6,7 @@ let router = new express.Router();
 
 router.get('/', async (req, res, next) => {
   try {
-    const results = await db.query(`SELECT * FROM companies`);
+    const results = await db.query(`SELECT code, name FROM companies`);
     return res.json({ companies: results.rows });
   } catch (e) {
     return next(e);
